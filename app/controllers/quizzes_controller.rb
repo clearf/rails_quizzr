67075ee817777dcbb7159f8f2edf9da4d840f940
omar delarosa <thedelarosa@gmail.com>
2013-07-16 14:50:20 -0400

    updated the new method in the quiz controller to include access to the questions model, updated my new_quiz view to render the correct results

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,10 +6,21 @@ class QuizzesController < ApplicationController
 
     def new
        @quiz = Quiz.new
+
+       3.times do
+            question = @quiz.questions.build
+        end
     end
 
     def create
-        
+        # puts "This is my params below:"
+        # puts params
+        quiz = Quiz.new(params[:quiz])
+        if quiz.save!
+            redirect_to quizzes_path
+        else
+            redirect_to :new
+        end
     end
 
     def show
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,5 @@
 class Quiz < ActiveRecord::Base
-  attr_accessible :name, :questions#, :questions_attributes
+  attr_accessible :name, :questions, :questions_attributes
   
   # #association 1
   has_many :questions, :dependent => :destroy
@@ -8,6 +8,6 @@ class Quiz < ActiveRecord::Base
   # has_many :answers, through: :questions
   
   # #nested attribute
-  # accepts_nested_attributes_for :questions
+  accepts_nested_attributes_for :questions
 
 end
