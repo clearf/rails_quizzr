5697caef8d97f1c7d226cffd0f3f29d618b54ac0
omar delarosa <thedelarosa@gmail.com>
2013-07-17 11:53:25 -0400

    added a link to quizzes index page that makes answerkey boxes appear

--- app/assets/javascripts/quizzr.js
+++ app/assets/javascripts/quizzr.js
@@ -1,3 +1,5 @@
+answer_keys = []
+
 function remove_field(link){
     var field_container = $($(link).parent());
     var hidden_field = $($(link).prev('input[type=hidden]'));
@@ -7,10 +9,16 @@ function remove_field(link){
     field_container.hide(200);
 }
 
-function show_me_an_(link) {
-
+function AnswerKey(element){
+    var answer_key = $('<div>');
+    answer_key.addClass('answer_key');
+    answer_key.text('answer key')
+    // var all_quizzes = $('.quiz');
+    console.log(element)
+    answer_key.insertAfter($(element))
+    // return answer_key
 }
 
 $(function(){
-    //nothing here yet
+
 })
