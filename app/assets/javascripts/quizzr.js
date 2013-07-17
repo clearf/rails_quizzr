aa9d6c57af447a825d8b08f9bae063e131862ed6
omar delarosa <thedelarosa@gmail.com>
2013-07-17 12:21:28 -0400

    added more attributes to the AnswerKey object in the js file

--- app/assets/javascripts/quizzr.js
+++ app/assets/javascripts/quizzr.js
@@ -1,4 +1,5 @@
-answer_keys = []
+//this array holds all currently instantiated AnswerKey elements
+var answer_keys = []
 
 function remove_field(link){
     var field_container = $($(link).parent());
@@ -9,14 +10,28 @@ function remove_field(link){
     field_container.hide(200);
 }
 
-function AnswerKey(element){
-    var answer_key = $('<div>');
-    answer_key.addClass('answer_key');
-    answer_key.text('answer key')
-    // var all_quizzes = $('.quiz');
-    console.log(element)
-    answer_key.insertAfter($(element))
-    // return answer_key
+function AnswerKey(link_that_calls_function,quiz_id){
+    var quiz_container_element = $($($(link_that_calls_function).parent()).parent());
+    var quiz_answers_elements = quiz_container_element
+                                    .children('.questions')
+                                    .children('.question')
+                                    .children()
+                                    .children('.correct')
+    var answer_key_element = $('<div>');
+    answer_key_element.addClass('answer_key');
+    answer_key_element.text('answer key')
+    
+    var answer_key_object = {
+        element: answer_key_element,
+        quiz: {
+            id: quiz_id,
+            container_element: quiz_container_element,
+            answers_elements: quiz_answers_elements
+        }
+    }
+    answer_key_element.insertAfter($(link_that_calls_function))
+    answer_keys.push(answer_key_object);
+    return answer_key_object
 }
 
 $(function(){
